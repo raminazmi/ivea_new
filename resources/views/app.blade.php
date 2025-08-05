117642b4114241663d438b0155fd9f2fa46cae27
ramialmallahi <raminazmi7@gmail.com>
2025-08-05 21:13:51 +0300

    commit "4"

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -73,15 +73,12 @@
     <!-- Scripts -->
     @routes
     @viteReactRefresh
-    @production
-    @php
-    $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-    @endphp
-    <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/js/app.tsx']['css'][0]) }}">
-    <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.tsx']['file']) }}"></script>
+    @if (app()->environment('production'))
+    <link rel="stylesheet" href="{{ mix('build/assets/app.css') }}">
+    <script src="{{ mix('build/assets/app.js') }}" defer></script>
     @else
-    @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
-    @endproduction
+    @vite(['resources/js/app.tsx'])
+    @endif
     @inertiaHead
 </head>
 
