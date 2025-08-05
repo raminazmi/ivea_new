5dc09e8240f0b6d1e0397f9835a8b7a282b6e6b1
ramialmallahi <raminazmi7@gmail.com>
2025-08-05 22:01:45 +0300

    commit "7"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AdminUserSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
             TestProductSeeder::class,
+            ProductFilterSeeder::class,
+            UpdateProductCustomizationSeeder::class,
+            ArticleSeeder::class,
+            ToolSeeder::class,
+            JobSeeder::class,
         ]);
     }
 }
