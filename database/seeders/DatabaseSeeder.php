7ed1f80d4a8af0a0b0ae83ccc39430ed06499f21
ramialmallahi <raminazmi7@gmail.com>
2025-09-11 22:37:11 +0300

    commit "36"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
             ToolSeeder::class,
             JobSeeder::class,
             ProjectSeeder::class,
-            LandingPageSeeder::class,
             PreparingForSummerSeeder::class,
         ]);
     }
